ae716b70451100608aa379fe799cb8c1689af13c
istiaqhaider8 <istiaqhaider8@gmail.com>
2022-08-02 21:37:14 +0600

    Layer Architecture

---
+++ UniversityAppMana/BLL/StudentManager.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using UniversityAppMana.Models;
+
+namespace UniversityAppMana.BLL
+{
+    public class StudentManager
+    {
+        string connectionString = @"Server = DESKTOP-CAJ7GNM; database = UniversityBD_29; Integrated Security = true";
+        public int InsertStudent(Student student)
+        {
+            SqlConnection connection = new SqlConnection(connectionString);
+            string query = "Insert into Students(Name,RegNo,Email,Address) VALUES('" + student.Name + "','" + student.RegNo +
+                           "','" + student.Email + "','" + student.Address + "');";
+            SqlCommand command = new SqlCommand(query, connection);
+            connection.Open();
+            int rowAffected = command.ExecuteNonQuery();
+            connection.Close();
+            return rowAffected;
+        }
+
+        public List<Student> GetAllStudents()
+        {
+            List<Student> studentList = new List<Student>();
+            SqlConnection connection = new SqlConnection(connectionString);
+            string query = "Select * from Students;";
+            SqlCommand command = new SqlCommand(query, connection);
+            connection.Open();
+            SqlDataReader reader = command.ExecuteReader();
+
+            while (reader.Read())
+            {
+                string name = reader["Name"].ToString();
+                string email = reader["Email"].ToString();
+                string regNo = reader["RegNo"].ToString();
+                string address = reader["Address"].ToString();
+                Student student = new Student(name, email, regNo, address);
+                studentList.Add(student);
+            }
+
+            reader.Close();
+            connection.Close();
+            return studentList;
+        }
+
+        public Student GetStudentByRegNo(string regNo)
+        {
+            SqlConnection connection = new SqlConnection(connectionString);
+            string query = "Select * from Students where RegNo='" + regNo + "';";
+            SqlCommand command = new SqlCommand(query, connection);
+            connection.Open();
+
+            Student student = null;
+            SqlDataReader reader = command.ExecuteReader();
+            if (reader.HasRows)
+            {
+                reader.Read();
+                string name = reader["Name"].ToString();
+                string email = reader["Email"].ToString();
+                string regNumber = reader["RegNo"].ToString();
+                string address = reader["Address"].ToString();
+                reader.Close();
+                student = new Student(name, email, regNumber, address);
+            }
+
+            connection.Close();
+            return student;
+        }
+
+        public bool IsRegNoExist(string regNo)
+        {
+            bool isRegNoExist = false;
+            Student student = GetStudentByRegNo(regNo);
+            if (student != null)
+            {
+                isRegNoExist = true;
+            }
+
+            return isRegNoExist;
+        }
+    }
+}
+
+
--- UniversityAppMana/UI/StudentEntryUI.aspx.cs
+++ UniversityAppMana/UI/StudentEntryUI.aspx.cs
@@ -2,11 +2,11 @@
 using System.Data;
 using System.Collections.Generic;
 using System.Data.SqlClient;
-
 using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using UniversityAppMana.BLL;
 
 using UniversityAppMana.Models;
 
@@ -15,13 +15,15 @@ namespace UniversityAppMana.UI
 {
     public partial class StudentEntryUI : System.Web.UI.Page
     {
-        string connectionString = @"Server = DESKTOP-1ACAIOK; database = UniversityBD_29; Integrated Security = true";
+
 
 
         protected void Page_Load(object sender, EventArgs e)
         {
 
         }
+
+        StudentManager studentManager = new StudentManager();
 
         protected void SaveButton_Click(object sender, EventArgs e)
         {
@@ -31,14 +33,14 @@ namespace UniversityAppMana.UI
             string address = addressTextBox.Text;
             Student student = new Student(name, email, regNo, address);
 
-            bool isRegNoExist = IsRegNoExist(student.RegNo);
+            bool isRegNoExist = studentManager.IsRegNoExist(student.RegNo);
             if (isRegNoExist)
             {
                 Response.Write("Registration number already exist");
             }
             else
             {
-                int rowAffected = InsertStudent(student);
+                int rowAffected = studentManager.InsertStudent(student);
 
                 if (rowAffected > 0)
                 {
@@ -50,94 +52,21 @@ namespace UniversityAppMana.UI
                 }
             }
         }
-
-
         protected void ShowButton_Click(object sender, EventArgs e)
         {
             ShowStudents();
         }
-
-
-        private int InsertStudent(Student student)
-        {
-            SqlConnection connection = new SqlConnection(connectionString);
-            string query = "Insert into Students(Name,RegNo,Email,Address) VALUES('" + student.Name + "','" + student.RegNo +
-                           "','" + student.Email + "','" + student.Address + "');";
-            SqlCommand command = new SqlCommand(query, connection);
-            connection.Open();
-            int rowAffected = command.ExecuteNonQuery();
-            connection.Close();
-            return rowAffected;
-        }
-
-
         private void ShowStudents()
         {
-            List<Student> studentList = GetAllStudents();
+            List<Student> studentList = studentManager.GetAllStudents();
 
 
             studentGridView.DataSource = studentList;
             studentGridView.DataBind();
         }
 
-        private List<Student> GetAllStudents()
-        {
-            List<Student> studentList = new List<Student>();
-            SqlConnection connection = new SqlConnection(connectionString);
-            string query = "Select * from Students;";
-            SqlCommand command = new SqlCommand(query, connection);
-            connection.Open();
-            SqlDataReader reader = command.ExecuteReader();
 
-            while (reader.Read())
-            {
-                string name = reader["Name"].ToString();
-                string email = reader["Email"].ToString();
-                string regNo = reader["RegNo"].ToString();
-                string address = reader["Address"].ToString();
-                Student student = new Student(name, email, regNo, address);
-                studentList.Add(student);
-            }
 
-            reader.Close();
-            connection.Close();
-            return studentList;
-        }
 
-        private Student GetStudentByRegNo(string regNo)
-        {
-            SqlConnection connection = new SqlConnection(connectionString);
-            string query = "Select * from Students where RegNo='"+regNo+"';";
-            SqlCommand command = new SqlCommand(query, connection);
-            connection.Open();
-
-            Student student = null;
-            SqlDataReader reader = command.ExecuteReader();
-            if (reader.HasRows)
-            {
-                reader.Read();
-                string name = reader["Name"].ToString();
-                string email = reader["Email"].ToString();
-                string regNumber = reader["RegNo"].ToString();
-                string address = reader["Address"].ToString();
-                reader.Close();
-                student = new Student(name, email, regNumber, address);
-            }
-
-            connection.Close();
-            return student;
-        }
-
-        private bool IsRegNoExist(string regNo)
-        {
-            bool isRegNoExist = false;
-            Student student = GetStudentByRegNo(regNo);
-            if (student != null)
-            {
-                isRegNoExist = true;
-            }
-
-            return isRegNoExist;
-        }
     }
 }
